20339c67767fcb9f5d7c153a2dd3558140a172d2
Baesiru <151522558+Baesiru@users.noreply.github.com>
2025-04-01 14:24:40 +0900

    댓글 DTO 검증 로직 추가

--- EditorBoard/src/main/java/com/baesiru/editorboard/controller/CommentController.java
+++ EditorBoard/src/main/java/com/baesiru/editorboard/controller/CommentController.java
@@ -5,6 +5,7 @@ import com.baesiru.editorboard.dto.comment.RequestCommentInfo;
 import com.baesiru.editorboard.dto.comment.RequestCommentUpdate;
 import com.baesiru.editorboard.dto.comment.ResponseComment;
 import com.baesiru.editorboard.service.CommentService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("/api/board/{boardId}/comment")
-    public ResponseEntity<Object> createComment(@PathVariable Long boardId, @RequestBody RequestComment requestComment) {
+    public ResponseEntity<Object> createComment(@PathVariable Long boardId, @RequestBody @Valid RequestComment requestComment) {
         commentService.createComment(boardId, requestComment);
         return ResponseEntity.ok("댓글이 성공적으로 작성되었습니다.");
     }
@@ -29,19 +30,19 @@ public class CommentController {
     }
 
     @PostMapping("/api/comment/{commentId}/delete")
-    public ResponseEntity<Object> deleteComment(@PathVariable Long commentId, @RequestBody RequestCommentInfo requestCommentInfo) {
+    public ResponseEntity<Object> deleteComment(@PathVariable Long commentId, @RequestBody @Valid RequestCommentInfo requestCommentInfo) {
         commentService.deleteComment(commentId, requestCommentInfo);
         return ResponseEntity.ok("댓글이 삭제되었습니다.");
     }
 
     @PostMapping("/api/comment/{commentId}/check")
-    public ResponseEntity<Object> checkComment(@PathVariable Long commentId, @RequestBody RequestCommentInfo requestCommentInfo) {
+    public ResponseEntity<Object> checkComment(@PathVariable Long commentId, @RequestBody @Valid RequestCommentInfo requestCommentInfo) {
         commentService.checkPassword(commentId, requestCommentInfo);
         return ResponseEntity.ok("비밀번호가 일치합니다.");
     }
 
     @PostMapping("/api/comment/{commentId}/update")
-    public ResponseEntity<Object> updateComment(@PathVariable Long commentId, @RequestBody RequestCommentUpdate requestCommentUpdate) {
+    public ResponseEntity<Object> updateComment(@PathVariable Long commentId, @RequestBody @Valid RequestCommentUpdate requestCommentUpdate) {
         commentService.updateComment(commentId, requestCommentUpdate);
         return ResponseEntity.ok("댓글이 수정되었습니다.");
     }
--- EditorBoard/src/main/java/com/baesiru/editorboard/dto/comment/RequestComment.java
+++ EditorBoard/src/main/java/com/baesiru/editorboard/dto/comment/RequestComment.java
@@ -1,5 +1,8 @@
 package com.baesiru.editorboard.dto.comment;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RequestComment {
+    @NotBlank(message = "필수 입력 항목입니다.")
     private String content;
+    @NotBlank(message = "필수 입력 항목입니다.")
+    @Size(min = 2, max = 20, message = "최소 2자, 최대 20자까지 입력 가능합니다.")
     private String username;
+    @NotBlank(message = "필수 입력 항목입니다.")
+    @Size(min = 2, max = 100, message = "최소 2자, 최대 100자까지 입력 가능합니다.")
     private String password;
     private Long parentId;
 }
